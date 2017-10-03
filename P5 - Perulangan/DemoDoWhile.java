c0387d3efa996e4bac5421d3b2b5b255291751ac
taufiq17 <32066322+taufiq17@users.noreply.github.com>
2017-10-03 16:25:29 +0700

    Add files via upload

---
+++ P5 - Perulangan/DemoDoWhile.java
@@ -0,0 +1,11 @@
+public class DemoDoWhile{
+
+    public static void main (String[] args) {
+        int x = 0;
+        do{
+            System.out.println(x);
+            x++;java
+        }while (x<10);
+    }
+
+} 
---
+++ P5 - Perulangan/DemoFor.java
@@ -0,0 +1,25 @@
+public class DemoFor {
+
+    public static void main(String[] args){
+        int i;
+        for( i = 0; i < 10; i++ ){
+            System.out.print(i);
+        }
+    
+    }
+
+}
+
+
+
+
+/**
+*1. Deklarasi class dg nama DemoFor
+*3. Deklarasi Method main utk menjalankan class DemoFor
+*4. Deklarasi variabel i dg tipe data int
+*5. Perintah perulangan for dg inisialisasi i=0, kondisi i <10, perubah i++
+*6. Mencetak variabel i
+*7. Penutup statement pd perulangan for
+*9. penutup method main
+*11.penutup class DemoFor
+ */
